62206b943ac9a99d34fd95282ea0add31eb42e19
Abhishek Waskar <abhishek@Abhisheks-MacBook-Air.local>
2025-02-05 20:32:08 +0530

    Added .gitignore - To ignore build/ dir

--- build/CMakeFiles/calculator.dir/src/calculator.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/calculator.dir/src/calculator.cpp.o: \
-  /Users/abhishek/HelloWorldApp/calculator/src/calculator.cpp \
-  /Users/abhishek/HelloWorldApp/calculator/include/calculator.h
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/abhishek/HelloWorldApp/calculator
-# Build directory: /Users/abhishek/HelloWorldApp/calculator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(CalculatorTests "/Users/abhishek/HelloWorldApp/calculator/build/calculator_test")
-set_tests_properties(CalculatorTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/abhishek/HelloWorldApp/calculator/CMakeLists.txt;18;add_test;/Users/abhishek/HelloWorldApp/calculator/CMakeLists.txt;0;")
